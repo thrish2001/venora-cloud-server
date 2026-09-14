3d13af001de5feb691b451ea05a0bdeb8026c2bc
Thirasha <thrishbandara@gmail.com>
2026-09-14 12:17:17 +0530

    feat: add sd-report route

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/login.html',      (req, res) => res.sendFile(path.join(__dirname, 'public', 'login.html')));
 app.get('/dashboard.html',  (req, res) => res.sendFile(path.join(__dirname, 'public', 'dashboard.html')));
 app.get('/production.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'production.html')));
+app.get('/sd-report.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'sd-report.html')));
 app.get('/sd-dashboard.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'sd-dashboard.html')));
 app.get('/comparison.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'comparison.html')));
+app.get('/sd-report.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'sd-report.html')));
 app.get('/sd-dashboard.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'sd-dashboard.html')));
 app.get('/comparison.html', (req, res) => res.sendFile(path.join(__dirname, 'public', 'comparison.html')));
 app.get('/upload.html',     (req, res) => res.sendFile(path.join(__dirname, 'public', 'upload.html')));
@@ -56,6 +58,7 @@ async function initDB() {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, async () => { await initDB(); console.log('Server on port ' + PORT); });
+
 
 
 
